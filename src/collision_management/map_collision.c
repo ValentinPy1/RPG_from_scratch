e02201cbf9cce4337f5345a4cc0136df93e28e94
Gaby <gabriel.lepinay@epitech.eu>
2022-04-20 10:18:34 +0200

    [ADD] Collision

--- src/collision_management/map_collision.c
+++ src/collision_management/map_collision.c
@@ -13,7 +13,6 @@ int parse_tile(map_t *map)
 
     for (row = 0; row < map->size_y; row++);
     for (col = 0; col < map->size_x; col++);
-    printf("LEN tile[%i][%i]\n", col, row);
 }
 
 int is_blocking_tile(map_t *map, sfVector2f pos)
@@ -28,10 +27,8 @@ int is_blocking_tile(map_t *map, sfVector2f pos)
     // printf("P relative pos:(%i, %i)\n", x / 32, y / 32);
     // printf("Current tile value:%i\n", map->tiles[y / 32][x / 32]);
     if (map->tiles[y / 32][x / 32] == 7) {
-        printf("Lava not good\n");
         return (1);
     } else {
-        printf("GOOD\n");
         return (0);
     }
 }
