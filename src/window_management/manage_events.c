f8ba13a546eadf5257361530aaf0b611d8134132
Valentin PY <valentin.py@epitech.eu>
2022-04-26 13:57:17 +0200

    add: quiting in game

--- src/window_management/manage_events.c
+++ src/window_management/manage_events.c
@@ -12,11 +12,11 @@ void event_handling(sfRenderWindow *window, data_t *game_data, scene_t *scene)
     sfEvent event;
     sfVector2i mouse_loc;
     partic_ll_t *node;
-    particle_param_t param = setup_blood_param();
+    particle_param_t param = setup_fire_param();
 
     mouse_loc = sfMouse_getPositionRenderWindow(window);
     while (sfRenderWindow_pollEvent(window, &event)) {
-        if (event.type == sfEvtClosed)
+        if (event.type == sfEvtClosed || event.key.code == sfKeyEscape)
             sfRenderWindow_close(window);
         manage_hover_buttons(game_data, scene->buttons, mouse_loc);
         if (event.type == sfEvtMouseButtonReleased) {
