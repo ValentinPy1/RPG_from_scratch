3fd07bd6e7bec1bc31644abb4b0c3d0735931d76
Paul THIEBAULT <paul.thiebault@epitech.eu>
2022-04-28 17:28:03 +0200

    add: option scene

--- include/contructors/construct_callbacks.h
+++ include/contructors/construct_callbacks.h
@@ -11,17 +11,19 @@
 
 typedef struct but_call_s {
     char *name;
-    int (*func)(button_t *button, char **scenes_name, int *index_run);
+    int (*func)(data_t *game_data, char **scenes_name, int *index_run);
 } but_call_t;
 
-int nothing(button_t *button, char **scenes_name, int *index_run);
-int play(button_t *button, char **scenes_name, int *index_run);
-int quit(button_t *button, char **scenes_name, int *index_run);
+int nothing(data_t *game_data, char **scenes_name, int *index_run);
+int play(data_t *game_data, char **scenes_name, int *index_run);
+int quit(data_t *game_data, char **scenes_name, int *index_run);
+int options(data_t *game_data, char **scenes_name, int *index_run);
 
-static const but_call_t BUT_CALL[4] = {
+static const but_call_t BUT_CALL[5] = {
     {"no", nothing},
     {"play", play},
     {"quit", quit},
+    {"options", options},
     {NULL, NULL}
 };
 
--- src/config_management/callbacks/button_callback1.c
+++ src/config_management/callbacks/button_callback1.c
@@ -7,12 +7,12 @@
 
 #include "callbacks.h"
 
-int nothing(button_t *button, char **scenes_name, int *index_run)
+int nothing(data_t *game_data, char **scenes_name, int *index_run)
 {
     return (0);
 }
 
-int play(button_t *button, char **scenes_name, int *index_run)
+int play(data_t *game_data, char **scenes_name, int *index_run)
 {
     for (int index = 0; scenes_name[index] != NULL; index++) {
         if (my_strcmp(scenes_name[index], "main_scene") == 1)
@@ -21,8 +21,17 @@ int play(button_t *button, char **scenes_name, int *index_run)
     return (0);
 }
 
-int quit(button_t *button, char **scenes_name, int *index_run)
+int quit(data_t *game_data, char **scenes_name, int *index_run)
 {
     *index_run = -1;
     return 0;
+}
+
+int options(data_t *game_data, char **scenes_name, int *index_run)
+{
+    for (int index = 0; scenes_name[index] != NULL; index++) {
+        if (my_strcmp(scenes_name[index], "options") == 1)
+            *index_run = index;
+    }
+    return (0);
 }
