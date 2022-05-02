a05ffc0c86af434e78d7f580553449ed0a220f00
Valentin PY <valentin.py@epitech.eu>
2022-05-02 17:41:21 +0200

    add: min distance

--- src/ennemies/update.c
+++ src/ennemies/update.c
@@ -18,32 +18,72 @@ float normalize(float dist, float max_speed)
     return dist;
 }
 
-float distance(sfVector2f p1, sfVector2f p2)
+float get_distance(sfVector2f p1, sfVector2f p2)
 {
-    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+    float distx = p2.x - p1.x;
+    float disty = p2.y - p1.y;
+
+    return sqrt(pow(distx, 2) + pow(disty, 2));
+}
+
+sfVector2f get_direction(sfVector2f p1, sfVector2f p2)
+{
+    int x = p2.x - p1.x;
+    int y = p2.y - p1.y;
+
+    return (sfVector2f) {x, y};
 }
 
 sfVector2f calculate_intent(data_t *gd, ennemies_t *node)
 {
-    sfVector2f intent = {(gd->red->pos.x - node->ennem.pos.x), (gd->red->pos.y - node->ennem.pos.y)};
-    float dist;
+    sfVector2f epos = node->ennem.pos;
+    sfVector2f ppos = {gd->red->pos.x, gd->red->pos.y - 15};
+    sfVector2f dir = get_direction(epos, ppos);
+    float dist = get_distance(epos, ppos);
+    float target = 30;
+    float spring = dist - target;
+    sfVector2f intent = {dir.x * spring, dir.y * spring};
+    sfVector2f tmpos;
+
+    target = 10;
     for (ennemies_t *temp = gd->ennemies->next; temp != NULL;
     temp = temp->next) {
-        dist = distance(temp->ennem.pos, node->ennem.pos);
+        tmpos = temp->ennem.pos;
+        dist = get_distance(epos, tmpos);
         if (dist != 0)
+            dir = get_direction(epos, tmpos);
+            spring = target / (target + pow(dist, 3)) * 4000;
             intent = (sfVector2f)
-            {intent.x - (temp->ennem.pos.x - node->ennem.pos.x) / pow(dist, 3) * 4000,
-            intent.y - (temp->ennem.pos.y - node->ennem.pos.y) / pow(dist, 3) * 4000};
+            {intent.x - (tmpos.x - epos.x) * spring,
+            intent.y - (tmpos.y - epos.y) * spring};
     }
     return intent;
 }
+
+// sfVector2f calculate_intent(data_t *gd, ennemies_t *node)
+// {
+//     sfVector2f intent = {(gd->red->pos.x - node->ennem.pos.x),
+//     (gd->red->pos.y - node->ennem.pos.y - 15)};
+//     float dist;
+//     for (ennemies_t *temp = gd->ennemies->next; temp != NULL;
+//     temp = temp->next) {
+//         dist = distance(temp->ennem.pos, node->ennem.pos);
+//         if (dist != 0)
+//             intent = (sfVector2f)
+//             {intent.x - (temp->ennem.pos.x - node->ennem.pos.x)
+//             / pow(dist, 3) * 4000,
+//             intent.y - (temp->ennem.pos.y - node->ennem.pos.y)
+//             / pow(dist, 3) * 4000};
+//     }
+//     return intent;
+// }
 
 void update_ennem_node(data_t *gd, ennemies_t *node)
 {
     sfVector2f ennem = {node->ennem.pos.x, node->ennem.pos.y};
     sfVector2f intent = calculate_intent(gd, node);
     float angle = atan(intent.y / intent.x);
-    float dist = sqrt(pow(intent.x, 2) + pow(intent.y, 2));
+    float dist = get_distance((sfVector2f) {0, 0}, intent);
     float speed = normalize(dist, node->ennem.speed);
     sfVector2f move = {speed * cos(angle), speed * sin(angle)};
 
