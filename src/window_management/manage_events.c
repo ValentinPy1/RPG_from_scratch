8839b05ed8fa83975f122060674ef987e31a939d
Valentin PY <valentin.py@epitech.eu>
2022-05-05 19:51:49 +0200

    add: space attack

--- src/window_management/manage_events.c
+++ src/window_management/manage_events.c
@@ -12,6 +12,26 @@
 
 float get_distance(sfVector2f p1, sfVector2f p2);
 
+static void push_enemies(data_t *gd)
+{
+    enemies_t *tmp;
+    sfVector2f ppos;
+    sfVector2f epos;
+
+    tmp = gd->enemies->next;
+    ppos = gd->red->pos;
+    while (tmp != NULL) {
+        epos = tmp->enem->pos;
+        if (get_distance(epos, ppos) < 60) {
+            tmp->enem->kb_speed = 10;
+            tmp->enem->kb_dir = atan2((ppos.y - epos.y), (ppos.x - epos.x)) + PI;
+            tmp->enem->life -= 34; // TODO stat player
+            spawn_enem_blood(gd, tmp->enem->pos);
+        }
+        tmp = tmp->next;
+    }
+}
+
 void kbd_input(data_t *gd, scene_t *scene, sfEvent event, sfVector2i mouse_loc)
 {
     if (event.key.code == sfKeyY)
@@ -21,6 +41,9 @@ void kbd_input(data_t *gd, scene_t *scene, sfEvent event, sfVector2i mouse_loc)
     if (event.key.code == sfKeyK)
         if (gd->enemies->next != NULL)
             destroy_next_enemies(gd->enemies);
+    if (event.key.code == sfKeySpace) {
+        push_enemies(gd);
+    }
 }
 
 static void init_hud(sfRenderWindow *window, data_t *game_data)
@@ -30,28 +53,6 @@ static void init_hud(sfRenderWindow *window, data_t *game_data)
 
     sfView_reset(game_data->red->view, view_rect);
     sfRenderWindow_setView(window, game_data->red->view);
-}
-
-static void push_enemies(data_t *gd)
-{
-    enemies_t *tmp;
-    sfVector2f ppos;
-    sfVector2f epos;
-
-    if (sfMouse_isButtonPressed(sfMouseRight)) {
-        tmp = gd->enemies->next;
-        ppos = gd->red->pos;
-        while (tmp != NULL) {
-            epos = tmp->enem->pos;
-            if (get_distance(epos, ppos) < 60) {
-                tmp->enem->kb_speed = 10;
-                tmp->enem->kb_dir = atan2((ppos.y - epos.y), (ppos.x - epos.x)) + PI;
-                tmp->enem->life -= 34; // TODO stat player
-                spawn_enem_blood(gd, tmp->enem->pos);
-            }
-            tmp = tmp->next;
-        }
-    }
 }
 
 static void events_conditions(sfEvent event, data_t *game_data,
@@ -67,9 +68,6 @@ scene_t *scene, sfVector2i mouse_loc)
     manage_hover_buttons(game_data, scene->buttons, mouse_loc);
     if (event.type == sfEvtMouseButtonReleased) {
         manage_clic_buttons(game_data, scene->buttons, mouse_loc);
-    }
-    if (event.type == sfEvtMouseButtonPressed) {
-        push_enemies(game_data);
     }
     if (event.type == sfEvtKeyPressed) {
         kbd_input(game_data, scene, event, mouse_loc);
